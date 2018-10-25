e01c65fbe2d853a50243db590edd056178fe9b67
Adam Wight <awight@wikimedia.org>
2018-10-24 17:57:35 -0700

    Test for JudgmentContentHandlerTest
    Bug: T206352
    Change-Id: I8aa07ac9c7ca8038168edc1a50b4201fe64fbe82

---
+++ tests/phpunit/Content/JudgmentContentHandlerTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace JADE\Tests\Content;
+
+use JADE\Tests\TestStorageHelper;
+use MediaWikiLangTestCase;
+use Title;
+use SearchEngine;
+use WikiPage;
+
+/**
+ * @group Database
+ * @group JADE
+ * @group medium
+ *
+ * @coversDefaultClass JADE\Content\JudgmentContentHandler
+ */
+class JudgmentContentHandlerTest extends MediaWikiLangTestCase {
+
+	/**
+	 * @covers ::getDataForSearchIndex
+	 */
+	public function testGetDataForSearchIndex() {
+		// Store a healthy judgment.
+		list( $entityPage, $entityRevision ) = TestStorageHelper::createEntity();
+		$judgmentTitle = Title::newFromDBkey( "Judgment:Revision/{$entityRevision->getId()}" );
+		$status = TestStorageHelper::saveJudgment(
+			$judgmentTitle->getDBkey(),
+			TestStorageHelper::getJudgmentText( 'revision' ) );
+		$this->assertTrue( $status->isOK() );
+
+		$judgmentPage = WikiPage::newFromId( $status->value['revision']->getPage() );
+		$parserOutput = $judgmentPage->getContent()->getParserOutput( $judgmentTitle );
+		$mockEngine = $this->createMock( SearchEngine::class );
+		$data = $judgmentPage->getContentHandler()->getDataForSearchIndex(
+			$judgmentPage, $parserOutput, $mockEngine );
+
+		// Has labels
+		$this->assertRegExp( '/C-class article/', $data['text'] );
+		// No HTML tags
+		$this->assertNotRegExp( '/</', $data['text'] );
+	}
+
+}
