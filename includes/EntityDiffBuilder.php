2561590d2e7f001b540e4cf8f89664388ddd5d97
DannyS712 <DannyS712.enwiki@gmail.com>
2020-04-06 21:12:43 +0000

    Remove use of Revision::newFromId, use RevisionRecord in Linker methods
    Bug: T249561
    Bug: T249021
    Change-Id: I2097d1403df6bd4bc17e6c5bd61fbbe35a2586f9

--- includes/EntityDiffBuilder.php
+++ includes/EntityDiffBuilder.php
@@ -26,11 +26,12 @@ use Html;
 use Linker;
 use MediaWiki\Linker\LinkRenderer;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Revision\RevisionLookup;
 use MediaWiki\Revision\RevisionRecord;
 use MediaWiki\Storage\NameTableAccessException;
 use RequestContext;
-use Revision;
 use Title;
+use TitleFormatter;
 
 /**
  * Create Diff header for Entity view.
@@ -47,11 +48,17 @@ class EntityDiffBuilder {
 	/** @var LinkRenderer */
 	protected $linkRenderer;
 
-	/** @var Revision */
-	protected $mNewRev;
+	/** @var RevisionLookup */
+	private $revLookup;
 
-	/** @var Revision */
-	protected $mOldRev;
+	/** @var TitleFormatter */
+	private $titleFormatter;
+
+	/** @var RevisionRecord */
+	private $mNewRevRecord;
+
+	/** @var RevisionRecord */
+	private $mOldRevRecord;
 
 	/** @var string|bool */
 	protected $mOldTags;
@@ -60,19 +67,23 @@ class EntityDiffBuilder {
 	protected $mNewTags;
 
 	public function __construct() {
-		$this->linkRenderer = MediaWikiServices::getInstance()->getLinkRenderer();
+		$services = MediaWikiServices::getInstance();
+		$this->linkRenderer = $services->getLinkRenderer();
+		$this->revLookup = $services->getRevisionLookup();
+		$this->titleFormatter = $services->getTitleFormatter();
 	}
 
 	public function getRevisionsFromTitle( Title $title ) {
 		$id = explode( '/', $title )[1];
-		$this->mNewRev = Revision::newFromId( (int)$id );
-		$this->mOldRev = $this->mNewRev->getPrevious();
+
+		$this->mNewRevRecord = $this->revLookup->getRevisionById( (int)$id );
+		$this->mOldRevRecord = $this->revLookup->getPreviousRevision( $this->mNewRevRecord );
 	}
 
 	public function buildDiffHeader( Title $title ) {
 		$this->getRevisionsFromTitle( $title );
 		$this->loadTags();
-		if ( $this->mOldRev !== null ) {
+		if ( $this->mOldRevRecord !== null ) {
 			$oldHeader = $this->buildOldHeader();
 		} else {
 			$oldHeader = '';
@@ -83,66 +94,77 @@ class EntityDiffBuilder {
 	}
 
 	public function buildOldHeader() {
-		if ( $this->mOldRev ) {
-				$prevlink = $this->linkRenderer->makeKnownLink(
-					$this->mOldRev->getTitle(),
-					$this->msg( 'previousdiff' )->text(),
-					[ 'id' => 'differences-prevlink' ],
-					[ 'diff' => 'prev', 'oldid' => $this->mOldRev->getId() ]
-				);
+		$oldRevRecord = $this->mOldRevRecord;
+		if ( $oldRevRecord ) {
+			$prevlink = $this->linkRenderer->makeKnownLink(
+				$oldRevRecord->getPageAsLinkTarget(),
+				$this->msg( 'previousdiff' )->text(),
+				[ 'id' => 'differences-prevlink' ],
+				[ 'diff' => 'prev', 'oldid' => $oldRevRecord->getId() ]
+			);
 		} else {
-				$prevlink = "\u{00A0}";
+			$prevlink = "\u{00A0}";
 		}
 
-		$ldel = $this->revisionDeleteLink( $this->mOldRev );
-		$oldRevisionHeader = $this->getRevisionHeader( $this->mOldRev, 'complete' );
+		// Previously it was assumed that mOldRev was a Revision, even though it could have
+		// been null, and this follows that behaviour, since the code was designed with
+		// the assumption that it wasn't null
+		// TODO fix me what should happen if the previous RevRecord is null? I guess
+		// the EntityDiffBuilder requires it to have existed?
+		'@phan-var RevisionRecord $oldRevRecord';
+
+		$ldel = $this->revisionDeleteLink( $oldRevRecord );
+		$oldRevisionHeader = $this->getRevisionHeader( $oldRevRecord, 'complete' );
 		$oldChangeTags = ChangeTags::formatSummaryRow( $this->mOldTags, 'diff', $this->getContext() );
 
+		$prefixedText = $this->titleFormatter->getPrefixedText( $oldRevRecord->getPageAsLinkTarget() );
 		$oldHeader = '<td class="diff-otitle" colspan="2">' .
-			'<div id="mw-diff-otitle6"><strong>' . $this->mOldRev->getTitle() . '</strong></div>' .
+			'<div id="mw-diff-otitle6"><strong>' . $prefixedText . '</strong></div>' .
 			'<div id="mw-diff-otitle1"><strong>' .
 			$oldRevisionHeader . '</strong></div>' .
 			'<div id="mw-diff-otitle2">' .
-			Linker::revUserTools( $this->mOldRev, false ) . '</div>' .
+			Linker::revUserTools( $oldRevRecord, false ) . '</div>' .
 			'<div id="mw-diff-otitle3">' .
-			Linker::revComment( $this->mOldRev, false, true ) . $ldel . '</div>' .
+			Linker::revComment( $oldRevRecord, false, true ) . $ldel . '</div>' .
 			'<div id="mw-diff-otitle5">' . $oldChangeTags[0] . '</div>' .
 			'<div id="mw-diff-otitle4">' . $prevlink . '</div></td>';
 		return $oldHeader;
 	}
 
 	public function buildNewHeader() {
-		if ( !$this->mNewRev->isCurrent() ) {
+		$newRevRecord = $this->mNewRevRecord;
+		if ( !$newRevRecord->isCurrent() ) {
 			$nextlink = $this->linkRenderer->makeKnownLink(
-				$this->mNewRev->getTitle(),
+				$newRevRecord->getPageAsLinkTarget(),
 				$this->msg( 'nextdiff' )->text(),
 				[ 'id' => 'differences-nextlink' ],
-				[ 'diff' => 'next', 'oldid' => $this->mNewRev->getId() ]
+				[ 'diff' => 'next', 'oldid' => $newRevRecord->getId() ]
 			);
 		} else {
 			$nextlink = "\u{00A0}";
 		}
 
-		if ( $this->mNewRev->isMinor() ) {
+		if ( $newRevRecord->isMinor() ) {
 			$newminor = ChangesList::flag( 'minor' );
 		} else {
 			$newminor = '';
 		}
 
 		# Handle RevisionDelete links...
-		$rdel = $this->revisionDeleteLink( $this->mNewRev );
+		$rdel = $this->revisionDeleteLink( $newRevRecord );
 
-		$newRevisionHeader = $this->getRevisionHeader( $this->mNewRev, 'complete' );
+		$newRevisionHeader = $this->getRevisionHeader( $newRevRecord, 'complete' );
 		$newChangeTags = ChangeTags::formatSummaryRow( $this->mNewTags, 'diff', $this->getContext() );
 
+		$prefixedText = $this->titleFormatter->getPrefixedText( $newRevRecord->getPageAsLinkTarget() );
 		$newHeader = '<td class="diff-ntitle" colspan="2">' .
-			'<div id="mw-diff-ntitle6"><strong>' . $this->mNewRev->getTitle() . '</strong></div>' .
+			'<div id="mw-diff-ntitle6"><strong>' . $prefixedText . '</strong></div>' .
 			'<div id="mw-diff-ntitle1"><strong>' .
 			$newRevisionHeader . '</strong></div>' .
-			'<div id="mw-diff-ntitle2">' . Linker::revUserTools( $this->mNewRev, true ) .
+			'<div id="mw-diff-ntitle2">' . Linker::revUserTools( $newRevRecord, true ) .
 			"</div>" .
 			'<div id="mw-diff-ntitle3">' . $newminor .
-			Linker::revComment( $this->mNewRev, true, true ) . $rdel . '</div>' .
+			Linker::revComment( $newRevRecord, true, true ) . $rdel . '</div>' .
 			'<div id="mw-diff-ntitle5">' . $newChangeTags[0] . '</div>' .
 			'<div id="mw-diff-ntitle4">' . $nextlink . '</div></td>';
 		return $newHeader;
@@ -151,22 +173,22 @@ class EntityDiffBuilder {
 	/**
 	 * Get a header for a specified revision.
 	 *
-	 * @param Revision $rev
+	 * @param RevisionRecord $revRecord
 	 * @param string $complete 'complete' to get the header wrapped depending
 	 *        the visibility of the revision and a link to edit the page.
 	 *
 	 * @return string HTML fragment
 	 */
-	public function getRevisionHeader( Revision $rev, $complete = '' ) {
+	private function getRevisionHeader( RevisionRecord $revRecord, $complete = '' ) {
 		$lang = $this->getLanguage();
 		$user = $this->getUser();
-		$revtimestamp = $rev->getTimestamp();
+		$revtimestamp = $revRecord->getTimestamp();
 		$timestamp = $lang->userTimeAndDate( $revtimestamp, $user );
 		$dateofrev = $lang->userDate( $revtimestamp, $user );
 		$timeofrev = $lang->userTime( $revtimestamp, $user );
 
 		$header = $this->msg(
-			$rev->isCurrent() ? 'currentrev-asof' : 'revisionasof',
+			$revRecord->isCurrent() ? 'currentrev-asof' : 'revisionasof',
 			$timestamp,
 			$dateofrev,
 			$timeofrev
@@ -176,15 +198,15 @@ class EntityDiffBuilder {
 			return $header->escaped();
 		}
 
-		$title = $rev->getTitle();
+		$title = $revRecord->getPageAsLinkTarget();
 
 		$header = $this->linkRenderer->makeKnownLink( $title, $header->text(), [],
-			[ 'oldid' => $rev->getId() ] );
+			[ 'oldid' => $revRecord->getId() ] );
 
-		if ( $this->userCanEdit( $rev ) ) {
+		if ( $this->userCanEdit( $revRecord ) ) {
 			$editQuery = [ 'action' => 'edit' ];
-			if ( !$rev->isCurrent() ) {
-				$editQuery['oldid'] = $rev->getId();
+			if ( !$revRecord->isCurrent() ) {
+				$editQuery['oldid'] = $revRecord->getId();
 			}
 
 			$key = MediaWikiServices::getInstance()->getPermissionManager()
@@ -197,7 +219,7 @@ class EntityDiffBuilder {
 				[ 'class' => 'mw-diff-edit' ],
 				$editLink
 			);
-			if ( $rev->isDeleted( RevisionRecord::DELETED_TEXT ) ) {
+			if ( $revRecord->isDeleted( RevisionRecord::DELETED_TEXT ) ) {
 				$header = Html::rawElement(
 					'span',
 					[ 'class' => 'history-deleted' ],
@@ -229,12 +251,13 @@ class EntityDiffBuilder {
 		// Load tags information for both revisions
 		$dbr = wfGetDB( DB_REPLICA );
 		$changeTagDefStore = MediaWikiServices::getInstance()->getChangeTagDefStore();
-		if ( $this->mOldRev === null ) {
+		if ( $this->mOldRevRecord === null ) {
 			$oldRevId = null;
 		} else {
-			$oldRevId = $this->mOldRev->getId();
+			$oldRevId = $this->mOldRevRecord->getId();
 		}
-		if ( $this->mOldRev !== false ) {
+		// FIXME $oldRevRecord is never false
+		if ( $this->mOldRevRecord !== false ) {
 			$tagIds = $dbr->selectFieldValues(
 				'change_tag',
 				'ct_tag_id',
@@ -253,10 +276,10 @@ class EntityDiffBuilder {
 		} else {
 			$this->mOldTags = false;
 		}
-		if ( $this->mNewRev === null ) {
+		if ( $this->mNewRevRecord === null ) {
 			$newRevId = null;
 		} else {
-			$newRevId = $this->mNewRev->getId();
+			$newRevId = $this->mNewRevRecord->getId();
 		}
 
 		$tagIds = $dbr->selectFieldValues(
@@ -304,13 +327,17 @@ class EntityDiffBuilder {
 	}
 
 	/**
-	 * @param Revision $rev
+	 * @param RevisionRecord $revRecord
 	 * @return bool whether the user can see and edit the revision.
 	 */
-	private function userCanEdit( Revision $rev ) {
+	private function userCanEdit( RevisionRecord $revRecord ) {
 		$user = $this->getUser();
 
-		if ( !$rev->userCan( RevisionRecord::DELETED_TEXT, $user ) ) {
+		if ( !$revRecord->audienceCan(
+			RevisionRecord::DELETED_TEXT,
+			RevisionRecord::FOR_THIS_USER,
+			$user
+		) ) {
 			return false;
 		}
 
@@ -318,12 +345,16 @@ class EntityDiffBuilder {
 	}
 
 	/**
-	 * @param Revision $rev
+	 * @param RevisionRecord $revRecord
 	 *
 	 * @return string
 	 */
-	protected function revisionDeleteLink( $rev ) {
-		$link = Linker::getRevDeleteLink( $this->getUser(), $rev, $rev->getTitle() );
+	private function revisionDeleteLink( RevisionRecord $revRecord ) {
+		$link = Linker::getRevDeleteLink(
+			$this->getUser(),
+			$revRecord,
+			$revRecord->getPageAsLinkTarget()
+		);
 		if ( $link !== '' ) {
 			$link = "\u{00A0}\u{00A0}\u{00A0}" . $link . ' ';
 		}
